fc16c6c65cb16717981f75bf615c3f47c76986b8
ammar <ammarashraf111@gmail.com>
2023-12-19 22:29:33 +0200

    Made the same previos change both doubly and circular lists

--- linked_lists/circular_linked_lists/main.c
+++ linked_lists/circular_linked_lists/main.c
@@ -23,17 +23,17 @@ C_LIST* clist_init()
 
 
 
-int get_clist_length(C_LIST** list)
+int get_clist_length(C_LIST* list)
 {
-	struct C_ListNode* walk = (*list)->head;
+	struct C_ListNode* walk = list->head;
 	int k=0;
-	if((*list)->head == NULL)
+	if(list->head == NULL)
 		return 0;
 
 	do{
 		walk = walk->next;
 		++k;
-	}while( walk != (*list)->head );
+	}while( walk != list->head );
 
 	return k;
 }
@@ -54,10 +54,10 @@ void print_clist(C_LIST* list)
 }
 
 
-void insert_node_at_end(C_LIST** list, int data)
+void insert_node_at_end(C_LIST* list, int data)
 {
 	/* walk pointer at the head */
-	struct C_ListNode* walk = (*list)->head;
+	struct C_ListNode* walk = list->head;
 
 	/* creatint the new node*/
 	struct C_ListNode* newNode = (struct C_ListNode*) malloc(sizeof(struct C_ListNode));
@@ -70,26 +70,26 @@ void insert_node_at_end(C_LIST** list, int data)
 	newNode->data = data;
 
 	/* checking if the list is empty */
-	if((*list)->head == NULL)
+	if(list->head == NULL)
 	{
 		newNode->next = newNode;// new node points to itself because it is going to be the only one in the list
-		(*list)->head = newNode;// head of the list points to the new node (only node)
+		list->head = newNode;// head of the list points to the new node (only node)
 	}else{
 	/* Here at least there is one node */
 		/* while condition checks if walk pointer is on the last element of the list */
-		while(walk->next != (*list)->head)
+		while(walk->next != list->head)
 		{
 			walk = walk->next;	// moving one step
 		}
 		walk->next = newNode;	// last element points to the new node
-		newNode->next = (*list)->head;	// new element becomes the last element and points back to the head of the list
+		newNode->next = list->head;	// new element becomes the last element and points back to the head of the list
 	}
 
-	(*list)->size += 1;	// increamenting the size of the list
+	list->size += 1;	// increamenting the size of the list
 }
 
 
-void insert_node_at_first(C_LIST** list, int data)
+void insert_node_at_first(C_LIST* list, int data)
 {
 	struct C_ListNode* newNode = (struct C_ListNode*)malloc(sizeof(struct C_ListNode));
 	if(newNode == NULL)
@@ -100,22 +100,22 @@ void insert_node_at_first(C_LIST** list, int data)
 
 	newNode->data = data;
 
-	struct C_ListNode* walk = (*list)->head;
-	if((*list)->head == NULL)
+	struct C_ListNode* walk = list->head;
+	if(list->head == NULL)
 	{
 		newNode->next = newNode;
-		(*list)->head = newNode;
+		list->head = newNode;
 
 	}else{
-		while(walk->next != (*list)->head)
+		while(walk->next != list->head)
 		{
 			walk = walk->next;
 		}
-		newNode->next = (*list)->head;
-		(*list)->head = newNode;
-		walk->next = (*list)->head;
+		newNode->next = list->head;
+		list->head = newNode;
+		walk->next = list->head;
 	}
-	(*list)->size += 1;
+	list->size += 1;
 
 }
 
@@ -129,16 +129,16 @@ void insert_node_at_first(C_LIST** list, int data)
 int main()
 {
 	C_LIST* myclist = clist_init();
-	insert_node_at_first(&myclist, 99);
-	insert_node_at_end(&myclist, 50);
-	insert_node_at_end(&myclist, 40);
-	insert_node_at_end(&myclist, 30);
-	insert_node_at_end(&myclist, 20);
+	insert_node_at_first(myclist, 99);
+	insert_node_at_end(myclist, 50);
+	insert_node_at_end(myclist, 40);
+	insert_node_at_end(myclist, 30);
+	insert_node_at_end(myclist, 20);
 
 
 	print_clist(myclist);
 
-	printf("%d \n" , get_clist_length(&myclist));
+	printf("%d \n" , get_clist_length(myclist));
 
 
 	return 0;
--- linked_lists/doubly_linked_list/main.c
+++ linked_lists/doubly_linked_list/main.c
@@ -16,49 +16,49 @@ typedef struct D_LIST{
 } D_LIST;
 
 
-void dlist_init(D_LIST** list)
+void dlist_init(D_LIST* list)
 {
-	(*list)->head = NULL;
-	(*list)->tail = NULL;
-	(*list)->size = 0;
+	list->head = NULL;
+	list->tail = NULL;
+	list->size = 0;
 }
 
 
 
-void delete_node(D_LIST** list, int position)
+void delete_node(D_LIST* list, int position)
 {
-	if((*list)->head == NULL)
+	if(list->head == NULL)
 		return;
 	struct D_ListNode* walk;
-	if((*list)->head == (*list)->tail)	// one element only
+	if(list->head == list->tail)	// one element only
 	{
-		walk = (*list)->head;
-		(*list)->head =	NULL;
-		(*list)->tail = NULL;
+		walk = list->head;
+		list->head =	NULL;
+		list->tail = NULL;
 		free(walk);
 	}else if( position == 0 ){
-		walk = (*list)->head;
-		(*list)->head = (*list)->head->next;
-		(*list)->head->prev = NULL;
+		walk = list->head;
+		list->head = list->head->next;
+		list->head->prev = NULL;
 		free(walk);
-	}else if( position >= (*list)->size-1 ){
-		walk = (*list)->tail;
-		(*list)->tail = (*list)->tail->prev;
-		(*list)->tail->next = NULL;
+	}else if( position >= list->size-1 ){
+		walk = list->tail;
+		list->tail = list->tail->prev;
+		list->tail->next = NULL;
 		free(walk);
 	}else{
 		int k = 0;
-		if( position <  ((*list)->size / 2 ) )
+		if( position <  (list->size / 2 ) )
 		{
-			walk = (*list)->head;
+			walk = list->head;
 			while( k < position )
 			{
 				walk = walk->next;
 				++k;
 			}
 		}else{
-			int new_pos = (*list)->size - position - 1;
-			walk = (*list)->tail;
+			int new_pos = list->size - position - 1;
+			walk = list->tail;
 			while( k < new_pos )
 			{
 				walk = walk->prev;
@@ -69,7 +69,7 @@ void delete_node(D_LIST** list, int position)
 		walk->next->prev = walk->prev;
 		free(walk);
 	}
-	(*list)->size -= 1;
+	list->size -= 1;
 }
 
 
@@ -80,7 +80,7 @@ void delete_node(D_LIST** list, int position)
 
 
 
-void insert_node(D_LIST** list, int data, int position)
+void insert_node(D_LIST* list, int data, int position)
 {
 	struct D_ListNode* newNode = (struct D_ListNode*)malloc(sizeof(struct D_ListNode));
 	if(newNode==NULL){
@@ -88,38 +88,38 @@ void insert_node(D_LIST** list, int data, int position)
 		return;
 	}
 	newNode->data = data;
-	if((*list)->head == NULL)		// empty case
+	if(list->head == NULL)		// empty case
 	{
-		(*list)->head = newNode;
-		(*list)->tail = newNode;
+		list->head = newNode;
+		list->tail = newNode;
 		newNode->next = NULL;
 		newNode->prev = NULL;
 	}else if( position == 0 )		// insert at first
 	{
 
-			newNode->next = (*list)->head;
-			(*list)->head->prev = newNode;
+			newNode->next = list->head;
+			list->head->prev = newNode;
 			newNode->prev = NULL;
-			(*list)->head = newNode;
-	}else if(position >= (*list)->size)
+			list->head = newNode;
+	}else if(position >= list->size)
 	{
-		newNode->prev = (*list)->tail;
-		(*list)->tail->next = newNode;
+		newNode->prev = list->tail;
+		list->tail->next = newNode;
 		newNode->next = NULL;
-		(*list)->tail = newNode;
+		list->tail = newNode;
 	}else {		// insert in the middle
 		struct D_ListNode *walk;
 		int k = 0;
-		if( position <  ((*list)->size / 2 ) ){
-			walk = (*list)->head;
+		if( position <  (list->size / 2 ) ){
+			walk = list->head;
 			while( k<position )
 			{
 				walk = walk->next;
 				++k;
 			}
 		}else{
-			int new_pos = (*list)->size - position - 1;
-			walk = (*list)->tail;
+			int new_pos = list->size - position - 1;
+			walk = list->tail;
 			while( k < new_pos )
 			{
 				walk = walk->prev;
@@ -132,7 +132,7 @@ void insert_node(D_LIST** list, int data, int position)
 		walk->prev = newNode;
 	}
 
-	(*list)->size += 1;
+	list->size += 1;
 }
 
 
@@ -155,20 +155,17 @@ void print_list(D_LIST* list)
 int main()
 {
 	D_LIST * mydlist = malloc(sizeof(D_LIST));
-	dlist_init(&mydlist);
+	dlist_init(mydlist);
 
-	insert_node(&mydlist, 10, 0);
-	insert_node(&mydlist, 20, 1);
-	insert_node(&mydlist, 30, 2);
-	insert_node(&mydlist, 15, 1);
+	insert_node(mydlist, 10, 0);
+	insert_node(mydlist, 20, 1);
+	insert_node(mydlist, 30, 2);
+	insert_node(mydlist, 15, 1);
 
-	insert_node(&mydlist, 79, 4);
+	insert_node(mydlist, 79, 4);
 
-	delete_node(&mydlist, 2 );
-	delete_node(&mydlist, 3 );
-	delete_node(&mydlist, 0 );
-	delete_node(&mydlist, 2 );
-	delete_node(&mydlist, 0 );
+	delete_node(mydlist, 0 );
+	delete_node(mydlist, 0 );
 
 	print_list(mydlist);
 
